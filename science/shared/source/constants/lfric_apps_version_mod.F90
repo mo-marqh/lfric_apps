335b3668cdbeb94468e86de9f03956dbd29ccfde
mo-marqh <87382490+mo-marqh@users.noreply.github.com>
2025-12-01 13:01:18 +0000

    Provide forecast reference time scalar in XIOS XML interface. XIOS3 compatibility

---
+++ science/shared/source/constants/lfric_apps_version_mod.F90
@@ -0,0 +1,15 @@
+!-----------------------------------------------------------------------------
+! (C) Crown copyright 2026 Met Office. All rights reserved.
+! The file LICENCE, distributed with this code, contains details of the terms
+! under which the code may be used.
+!-----------------------------------------------------------------------------
+!> @brief Module containing the lfric_apps version string
+!>
+
+MODULE lfric_apps_version_mod
+
+IMPLICIT NONE
+
+CHARACTER(LEN=*), PARAMETER :: lfric_apps_version_char = 'vn3.1.1_dev'
+
+END MODULE lfric_apps_version_mod
